1e31b3347d104a04eeb50c61a5ea58d57041607f
Julien Blin <julien@alleycorpnord.com>
2023-01-12 21:29:29 +0000

    feat(core): :sparkles: add small `utcNow` utility
     + support for setting the time during tests

---
+++ packages/core/r4b/date.ts
@@ -0,0 +1,24 @@
+let fakedSystemTime: Date | null = null;
+
+/**
+ * Returns a UTC date representing the current timestamp.
+ * Can be overloaded by using the setSystemTime method.
+ *
+ * Remarks: we chose to use this instead of a patching / fake approach (e.g. jest.setSystemTime) because doing so
+ * increase the test compilation time significantly. This is simpler, easier, and works just as well.
+ */
+export const utcNow = (): Date => fakedSystemTime || new Date();
+
+/**
+ * Override the system time returned by utcNow with a fixed value.
+ */
+export const setSystemTime = (
+  date: Date | string | number | null | undefined
+) => {
+  if (!date) {
+    fakedSystemTime = null;
+    return;
+  }
+
+  fakedSystemTime = new Date(date);
+};
