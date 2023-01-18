3f3a683fb39b276091a216daa091abf9775a64cd
Julien Blin <julien@alleycorpnord.com>
2023-01-18 16:26:17 +0000

    fix(medplum): :bug: fix meplum client adapter to return `undefined` instead of throwing

---
+++ packages/medplum/r4b/medplum-fhir-restful-client-adapter.test.ts
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { build } from "@bonfhir/core/r4b";
+import { MedplumClient } from "@medplum/core";
+import fetch from "fetch-vcr";
+import { existsSync } from "node:fs";
+import { mkdir } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { buildFhirRestfulClientAdapter } from "./medplum-fhir-restful-client-adapter";
+
+describe("medplum-fhir-restful-client-adapter", () => {
+  const medplum = new MedplumClient({
+    baseUrl: process.env.MEDPLUM_SERVER_URL || "http://medplum:8103",
+    fetch: fetch,
+  });
+  const client = buildFhirRestfulClientAdapter(medplum);
+  global.fetch = fetch as any;
+
+  beforeEach(async () => {
+    const fixturePath = join(
+      dirname(fileURLToPath(import.meta.url)),
+      "__fixtures__",
+      "npiregistry-sync",
+      expect.getState().currentTestName!.replace(/[^a-z0-9]/gi, "_")
+    );
+    if (!existsSync(fixturePath)) {
+      await mkdir(fixturePath, { recursive: true });
+    }
+    fetch.configure({
+      fixturePath,
+      // mode: "record",
+    });
+
+    // Uncomment the following lines when re-recording fetches.
+    // await medplum.startClientLogin(
+    //   process.env.MEDPLUM_CLIENT_ID!,
+    //   process.env.MEDPLUM_CLIENT_SECRET!
+    // );
+  });
+
+  describe("read", () => {
+    it("return undefined when not found", async () => {
+      const result = await client.read("Patient", "12345");
+      expect(result).toBeUndefined();
+      const patient = await client.create(build("Patient", {}));
+      await client.delete("Patient", patient.id!);
+      const result2 = await client.read("Patient", patient.id!);
+      expect(result2).toBeUndefined();
+    });
+  });
+
+  describe("vread", () => {
+    it("return undefined when not found", async () => {
+      const result = await client.vread("Patient", "12345", "1");
+      expect(result).toBeUndefined();
+      const patient = await client.create(build("Patient", {}));
+      await client.delete("Patient", patient.id!);
+      const result2 = await client.vread(
+        "Patient",
+        patient.id!,
+        patient.meta!.versionId!
+      );
+      expect(result2).toBeUndefined();
+    });
+  });
+
+  describe("delete", () => {
+    it("doest not throw when when not found", async () => {
+      await expect(client.delete("Patient", "54321")).resolves.toBeUndefined();
+      const patient = await client.create(build("Patient", {}));
+      await client.delete("Patient", patient.id!);
+      await expect(
+        client.delete("Patient", patient.id!)
+      ).resolves.toBeUndefined();
+    });
+  });
+});
