f0e84553e77a518f52af193e434e3859727fb2e5
Louis-Philippe Perron <32278+lp@users.noreply.github.com>
2023-01-31 19:41:12 +0000

    feat(refine-fhir-dataprovider): refine fhir dataprovider initial setup

---
+++ packages/refine-fhir-dataprovider/README.md
@@ -0,0 +1,3 @@
+Refine CRUD builder FHIR data provider
+
+See https://bonfhir.dev/packages/integrations/refine for more information.
---
+++ packages/refine-fhir-dataprovider/package.js
@@ -0,0 +1,72 @@
+/**
+ * This script is a helper to create the final NPM package.
+ */
+
+import { existsSync } from "fs";
+import { copyFile, readFile, rename, unlink, writeFile } from "fs/promises";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const execAsync = promisify(exec);
+
+(async () => {
+  const rootPackageDirectory = dirname(fileURLToPath(import.meta.url));
+  const distDirectory = join(rootPackageDirectory, "dist");
+
+  // Copy package.json while stripping unwanted information
+  const packageJson = JSON.parse(
+    await readFile(join(rootPackageDirectory, "package.json"), "utf8")
+  );
+  delete packageJson.scripts;
+  delete packageJson.packageManager;
+  delete packageJson.devDependencies;
+  delete packageJson.prettier;
+
+  await writeFile(
+    join(distDirectory, "package.json"),
+    JSON.stringify(packageJson),
+    "utf8"
+  );
+
+  // Copy README.md
+  await copyFile(
+    join(rootPackageDirectory, "README.md"),
+    join(distDirectory, "README.md")
+  );
+
+  // Delete TypeScript build info
+  try {
+    await unlink(join(distDirectory, "tsconfig.build.tsbuildinfo"));
+  } catch {
+    // Ignore the error if the file does not exists.
+  }
+
+  // eslint-disable-next-line no-undef
+  const npmCommand = process.argv[2] || "pack";
+  // eslint-disable-next-line no-undef
+  const npmCommandOptions = process.argv.slice(3).join(" ");
+
+  // Run npm in the dist directory
+  const result = await execAsync(`npm ${npmCommand} ${npmCommandOptions}`, {
+    cwd: distDirectory,
+    maxBuffer: 1024 * 1000 * 10,
+  });
+
+  // eslint-disable-next-line no-undef
+  console.log(result.stdout);
+  // eslint-disable-next-line no-undef
+  console.error(result.stderr);
+
+  // Copy the package back to root if was produced
+  const packageFilename = `${packageJson.name
+    .replace("@", "")
+    .replace("/", "-")}-${packageJson.version}.tgz`;
+  if (existsSync(join(distDirectory, packageFilename))) {
+    await rename(
+      join(distDirectory, packageFilename),
+      join(rootPackageDirectory, packageFilename)
+    );
+  }
+})();
