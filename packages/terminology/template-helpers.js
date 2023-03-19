5baad392dd8d0889e8fbbf5c98453b6e82cf84b0
Julien Blin <julien@alleycorpnord.com>
2023-03-19 13:43:55 +0000

    refactor(terminology): :recycle: remove lodash code
    #158

--- packages/terminology/template-helpers.js
+++ packages/terminology/template-helpers.js
@@ -1,8 +1,10 @@
-import _ from "lodash";
-
 export function getCodeSystems(data) {
-  return _.uniqBy(
-    Object.values(data).filter((x) => x.resourceType === "CodeSystem"),
-    "name"
+  const allCodeSystems = Object.values(data).filter(
+    (x) => x.resourceType === "CodeSystem"
+  );
+
+  // uniqBy name
+  return allCodeSystems.filter(
+    (x, i, self) => i === self.findIndex((y) => x.name === y.name)
   );
 }
