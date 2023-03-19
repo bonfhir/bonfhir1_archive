2765f605de5d202912d2365efd9b0294ef2e172f
Julien Blin <julien@alleycorpnord.com>
2023-03-19 13:20:25 +0000

    refactor(cmsdotgov): :recycle: remove lodash code
    #158

--- packages/cmsdotgov/r4b/hcpcs/hcpcs-codes-builder.ts
+++ packages/cmsdotgov/r4b/hcpcs/hcpcs-codes-builder.ts
@@ -2,7 +2,6 @@ import { buildCodeableConcept } from "@bonfhir/core/r4b";
 import { CodeSystemURIs } from "@bonfhir/terminology/r4b";
 import { parse } from "csv-parse";
 import { CodeableConcept, Coding } from "fhir/r4";
-import isEmpty from "lodash/isEmpty";
 import { createReadStream } from "node:fs";
 
 export interface HCPCSCodesBuilderOptions {
@@ -71,7 +70,7 @@ export class HCPCSCodesBuilder {
   }
 
   private async ensureDataLoaded() {
-    if (!isEmpty(this._descriptionsByCode)) {
+    if (this._descriptionsByCode.size) {
       return;
     }
 
