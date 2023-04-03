64ccb644936bd8955653659f8b980d3e3bfa3bc1
Nicholas Gelinas <nick.gelinas@gmail.com>
2023-04-03 17:39:33 +0000

    test(test-support): :white_check_mark: test unknown resource type creation in `fake()`
    This originally came out of some code formatting causing a decrease in
    code coverage. Rather than bypass the nuissance, I decided to find some
    lines that were missed and test them. I discovered however that jest's
    coverage report (at least for `test-support`) shows the wrong line
    numbers. This appears to be an issue with `esbuild-jest`[0] so I suspect
    other packages will need their Jest configs updated as well even though
    the use case is pretty small.
    
    [0]: https://github.com/aelbore/esbuild-jest/issues/79

--- packages/test-support/r4b/fakes.test.ts
+++ packages/test-support/r4b/fakes.test.ts
@@ -3,6 +3,16 @@ import { DomainResource } from "fhir/r4";
 import { fake } from "./fakes";
 
 describe("fakes", () => {
+  it("throws an `Error` when attempting to generate an unrecognized resource type", () => {
+    const unrecognizedResourceTypeName = `BonfhirSpringTestExampleStructThingy`;
+
+    const generatingAnUnknownResourceType = () =>
+      // @ts-expect-error as this explicitly tests using an invalid type
+      fake(unrecognizedResourceTypeName);
+
+    expect(generatingAnUnknownResourceType).toThrowError(Error);
+  });
+
   it.each(DomainResourceTypes)("generate fakes for %s", (resourceType) => {
     const resource = fake(resourceType as ResourceType);
     expect((resource as DomainResource).text?.div).not.toBeFalsy();
